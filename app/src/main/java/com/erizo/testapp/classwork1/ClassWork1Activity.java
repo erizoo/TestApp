c58ea9a2cf655a7e376295051b6bc251e124fcf0
Erizo <alexboiko1993@gmail.com>
2018-02-06 09:15:47 +0300

    add for first lesson

---
+++ app/src/main/java/com/erizo/testapp/classwork1/ClassWork1Activity.java
@@ -0,0 +1,55 @@
+package com.erizo.testapp.classwork1;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+
+import com.erizo.testapp.R;
+
+/**
+ * Created by Erizo on 05.02.2018.
+ */
+
+public class ClassWork1Activity extends AppCompatActivity {
+
+    private final static String TAG = ClassWork1Activity.class.getSimpleName();
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.class_work_1_activity);
+        Log.e(TAG, "onCreate");
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.e(TAG, "onStart");
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.e(TAG, "onResume");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.e(TAG, "onPause");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.e(TAG, "onStop");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.e(TAG, "onDestroy");
+    }
+
+}
