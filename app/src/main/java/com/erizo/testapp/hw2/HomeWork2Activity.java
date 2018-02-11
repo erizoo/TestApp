513f7eab2817b1a6e66e4d6c771e919a074eb772
erizo_s <alexboiko1993@gmail.com>
2018-02-11 14:53:17 +0300

    add homework two

--- app/src/main/java/com/erizo/testapp/hw2/HomeWork2Activity.java
+++ app/src/main/java/com/erizo/testapp/hw2/HomeWork2Activity.java
@@ -1,5 +1,6 @@
 package com.erizo.testapp.hw2;
 
+import android.graphics.Color;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
@@ -16,5 +17,6 @@ public class HomeWork2Activity extends AppCompatActivity {
     public void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.home_work_2_activity);
+        getWindow().getDecorView().setBackgroundColor(Color.LTGRAY);
     }
 }
